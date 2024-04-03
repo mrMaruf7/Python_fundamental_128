8c771b6d459fdac4d4f2e6285179a41cac126e72
Mustafa Maruf <109459343+mrMaruf7@users.noreply.github.com>
2024-04-03 23:37:28 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Understanding the variables in Python:
+Rules for declaring the variables in python:
+Rule no. 1: No namespace while Declaring  the Variable name
+Rule no. 2: A variable name should never start with a number
+Rule no. 3: A variable name should not contain any special characters(@#$%^&*)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to datatypes:
+1.	Strings-> str
+2.	Numbers(Intergers => int & Floats [Decimals =>0.1 ,0.2]) == float
+3.	Lists => list
+4.	Tuples => tuple
+5.	Dictionaries => dict
+Classification of Datatypes:
+Broadly Datatypes are classified into 2 categories:
+1.	Mutable Datatypes => which we can edit / alter => flexible in nature
+2.	Imutable Datatypes => which we cannot  edit / alter => fixed in nature
+Introduction to string datatype:
+Definition: A string is nothing but a series of characters declared in quotes.
+Classification it is classified as an immutable datatype
+How to define the string datatype:
+1. Single quotes
+2. Double quotes
+
+
+# In[1]:
+
+
+Introduction to String methods:
+
+
+# In[4]:
+
+
+name="Mustafa"
+name2="Mustafa2"
+type(name)
+type(name2)
+
+
+# In[5]:
+
+
+fullname = "joseph anthony"
+print(fullname)
+print(fullname.title())
+#req: i want the entire name in capitals...
+print(fullname.upper())
+#req: i want the entire name in small case letters...
+print(fullname.lower())
+
+
+
+# In[ ]:
+
+
+
+
