a4471df05899ff1a84f5ca1d0d49fb86cb11a244
Mustafa Maruf <109459343+mrMaruf7@users.noreply.github.com>
2024-04-08 23:05:36 +0530

    Day 4 Update

---
+++ Python (Day4).py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Understanding the variables in Python:
+Rules for declaring the variables in python:
+Rule no. 1: No namespace while Declaring  the Variable name
+Rule no. 2: A variable name should never start with a number
+Rule no. 3: A variable name should not contain any special characters(@#$%^&*)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to datatypes:
+1.	Strings-> str
+2.	Numbers(Intergers => int & Floats [Decimals =>0.1 ,0.2]) == float
+3.	Lists => list
+4.	Tuples => tuple
+5.	Dictionaries => dict
+Classification of Datatypes:
+Broadly Datatypes are classified into 2 categories:
+1.	Mutable Datatypes => which we can edit / alter => flexible in nature
+2.	Imutable Datatypes => which we cannot  edit / alter => fixed in nature
+Introduction to string datatype:
+Definition: A string is nothing but a series of characters declared in quotes.
+Classification it is classified as an immutable datatype
+How to define the string datatype:
+1. Single quotes
+2. Double quotes
+
+
+# In[1]:
+
+
+Introduction to String methods:
+
+
+# In[4]:
+
+
+name="Mustafa"
+name2="Mustafa2"
+type(name)
+type(name2)
+
+
+# In[5]:
+
+
+fullname = "joseph anthony"
+print(fullname)
+print(fullname.title())
+#req: i want the entire name in capitals...
+print(fullname.upper())
+#req: i want the entire name in small case letters...
+print(fullname.lower())
+
+
+
+# In[ ]:
+
+
+continuation with strings :
+understanding the concept of f strings :
+
+
+# In[ ]:
+
+
+# Genral syntax of f strings :
+f"custom message {placeholder -1} {placeholder-2}... {placeholder...N}"
+
+
+# In[1]:
+
+
+firstname = "pradeep"
+lastname = "kumar"
+#req: i want to get the full name.....?
+fullname = f"{firstname} {lastname}"
+print(fullname)
+print(fullname.title())
+message = f"keep up the good work, {fullname.title()}"
+print(message)
+
+
+# In[2]:
+
+
+Adding whitspace to strings:
+
+
+# In[ ]:
+
+
+print("Programming_languages:PyhtonJavaC++C")
+
+
+# In[ ]:
+
+
+#\n -------------> Printing the code on next line
+
+
+# In[ ]:
+
+
+print("Programming_languages:\nPyhton\nJava\nC++\nC")
+
+
+# In[ ]:
+
+
+#\t -------------> Printing the code After tabbing
+
+
+# In[ ]:
+
+
+print("Programming_languages:\n\tPyhton\n\tJava\n\tC++\n\tC")
+
+
+# In[ ]:
+
+
+Removing blank space from the string:
+
+
+# In[9]:
+
+
+name="      Python"
+name1="Pyhton         "
+name2="Python"
+name3="        Python          "
+print(name+"\n"+name1+"\n"+name2)
+name.lstrip()
+name1.rstrip()
+name3.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to list datatype:
+    Defination: A list is a collection of items declared in a particular order.
+    Classification: It is classified as a multable datatype.
+    How to define / declared the list dataypes...[]
+
+
+# In[3]:
+
+
+students=["Mustafa","Neha","Namrata","Sakshi"]
+print(students)
+type(students)
+print(students[1])
+print(students[2].title())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to indexing:
+
+
+# In[4]:
+
+
+1. How to add new elements to the list.
+2. How to Modify the elements to the list.
+3. How to delete an element form a list.
+
+
+# In[5]:
+
+
+students.append("Naman")
+print(students)
+
+
+# In[7]:
+
+
+students.insert(5,"Jyoti")
+print(students)
+
+
+# In[8]:
+
+
+students.modify(2,"Joel")
+print(students)
+
+
+# In[9]:
+
+
+students[1]="Joel"
+print(students)
+
+
+# In[10]:
+
+
+del students[5]
+print(students)
+
+
+# In[11]:
+
+
+x=students.pop(2)
+print(students)
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Continuation with list Datatype:
+Organisating the list datatype:
+
+
+# In[2]:
+
+
+cars= ["BMW","KIA","BENZ","SUSUKI","AUDI","TOYOTA"]
+print(cars)
+type(cars)
+
+
+# In[ ]:
+
+
+# i want to organise the list in alphabrtical order
+There are 2 types of approch:
+    1. Temp app= Sorted = The order is impliend temporary
+    2. Permanet App= Sort= Permanent changes are made in a list
+
+
+# In[4]:
+
+
+print(sorted(cars))
+print(cars)
+cars.sort()
+print(cars)
+
+
+# In[ ]:
+
+
+# Interview Q: Whats the difference between Sort and Sorted?
+
+
+# In[5]:
+
+
+# How to reverse a list permanently
+cars.reverse()
+print(cars)
+
+
+# In[8]:
+
+
+# Want to count the total numbers of elements in the list:
+print("Total no. of elements in the list are:")
+len(cars)
+
+
+# In[10]:
+
+
+#Intoduction to Slicing:
+#general syntax of slicing:
+#(start_value:stop_value:step_count)
+#Note: Stop_values will be always exculsive, to inculde we have to increment the index by +1
+
+student1= ['Mustafa','Aaron','Varad','Harsh','Akshay','Fardin']
+print(student1)
+
+
+# In[14]:
+
+
+#Slicing the list Student1 temporary
+print(student1[0:3])
+print(student1[0:6:3])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
